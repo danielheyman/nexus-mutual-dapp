4742a373736453b5f28e1af1b9c9986be22fa04c
Daniel Heyman <daniel.heyman@gmail.com>
2019-12-22 13:17:50 -0500

    Add ropsten support

--- src/App.js
+++ src/App.js
@@ -4,40 +4,78 @@ import { ApolloProvider, Query } from 'react-apollo';
 import {
   LinearProgress,
   Typography,
+  Toolbar,
+  AppBar,
+  Button,
+  makeStyles,
 } from '@material-ui/core';
 import './App.css';
 import Error from './components/Error';
 import InsuredContracts from './components/InsurecContracts';
+import { GRAPHQL_ENDPOINTS } from './config';
 
-if (!process.env.REACT_APP_GRAPHQL_ENDPOINT) {
-  throw new Error('REACT_APP_GRAPHQL_ENDPOINT environment variable not defined')
-}
-
-const client = new ApolloClient({
-  uri: process.env.REACT_APP_GRAPHQL_ENDPOINT,
-  cache: new InMemoryCache(),
-});
-
-const CONTRACTS_QUERY = gql`
-  query insuredContracts {
-    insuredContracts(first: 1000) {
-      id
-      ens
-      covers {
-        id
-        amount
-      }
-      stakes {
-        id
-        amount
-      }
-    }
-  }
-`
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 export default function App() {
+  const [network, setNetwork] = React.useState('mainnet');
+  const classes = useStyles();
+  const isMainnet = network === 'mainnet';
+
+  const client = new ApolloClient({
+    uri: isMainnet ? GRAPHQL_ENDPOINTS.mainnet : GRAPHQL_ENDPOINTS.ropsten,
+    cache: new InMemoryCache(),
+  });
+  
+  const CONTRACTS_QUERY = gql`
+    query insuredContracts {
+      insuredContracts(first: 1000) {
+        id
+        ens
+        covers {
+          id
+          amount
+          status
+          claims {
+            id
+            submitDate
+            statusUpdateDate
+            status
+            verdict
+            voteCount
+            votes {
+              submitDate
+              verdict
+            }
+          }
+        }
+        stakes {
+          id
+          amount
+        }
+      }
+    }
+  `;
+
   return (
     <ApolloProvider client={client}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Nexus Mutual ({ network })
+          </Typography>
+          { isMainnet
+            ? <Button color="inherit" onClick={() => setNetwork('ropsten')}>Switch To Ropsten</Button>
+            : <Button color="inherit" onClick={() => setNetwork('mainnet')}>Switch To Mainnet</Button>
+          }
+        </Toolbar>
+      </AppBar>
       <div className="App">
         <Query query={CONTRACTS_QUERY}>
           {({ data, error, loading }) => {
---
+++ src/config.js
@@ -0,0 +1,4 @@
+export const GRAPHQL_ENDPOINTS = {
+  mainnet: "https://api.thegraph.com/subgraphs/name/danielheyman/nexus-mutual",
+  ropsten: "https://api.thegraph.com/subgraphs/name/danielheyman/nexus-mutual-ropsten",
+};
